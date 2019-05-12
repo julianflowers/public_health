dc844c794128132fdefb8a32a04d11d6338ada5e
Julian Flowers <julian.flowers12@gmail.com>
2019-05-12 17:58:38 +0100

    updates

--- pubhealthScrapers/R/page_viz.R
+++ pubhealthScrapers/R/page_viz.R
@@ -1,28 +1,28 @@
 ## Page visualisation highlighting keywords
-## 
+##
 #
 
 #' Visualise text pages with keywords highlighted as a page plot
 #'
-#' @param text 
-#' @param keyword 
-#' @param title 
+#' @param text
+#' @param keyword
+#' @param title
 #'
 #' @return A page visualisation where selected keywords are highlighted
-#' 
+#'
 #'
 #' @examples
-#' 
+#'
 #' page_viz(text, keyword = "fingertips", title = "Mentions of Fingertips in document")
-#' 
+#'
 page_viz <- function(text, keyword, title = "Title"){
-  
+
   require(ggpage)
-  
+
   ggpage_build(text) %>%
     mutate(long_word = stringr::str_detect(word, keyword)) %>%
     ggpage_plot(aes(fill = long_word)) +
-    labs(title = "Title") +
+    labs(title = title) +
     scale_fill_manual(values = c("grey70", "blue"),
                       labels = c( "text", keyword),
                       name = "Keyword")
